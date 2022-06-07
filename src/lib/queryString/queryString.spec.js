26dbb3be3278ae01966a7849877990c79ed8aca7
Victor Campos <campos.viictor@gmail.com>
2022-06-06 23:25:57 -0300

    create more tests scenarios to queryString lib & refactor it

--- src/lib/queryString/queryString.spec.js
+++ src/lib/queryString/queryString.spec.js
@@ -9,4 +9,27 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Victor&profession=developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Victor',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Victor&abilities=JS,TDD');
+  });
+
+  it('should thrown an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Victor',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
